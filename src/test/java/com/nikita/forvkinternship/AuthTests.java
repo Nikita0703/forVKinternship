0d64a313fe2ab384ed37f42b40088de339bc6efb
Nikita0703 <nikita.noviczenko2005@gmail.com>
2024-03-12 16:43:30 +0300

    cleaning of the code

--- src/test/java/com/nikita/forvkinternship/AuthTests.java
+++ src/test/java/com/nikita/forvkinternship/AuthTests.java
@@ -9,6 +9,7 @@ import com.nikita.forvkinternship.security.JWTTokenProvider;
 import com.nikita.forvkinternship.services.UsersService;
 import com.nikita.forvkinternship.validation.ResponseErrorValidation;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
@@ -16,9 +17,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
 
 import org.springframework.security.core.Authentication;
@@ -28,10 +35,14 @@ import java.security.Principal;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(MockitoJUnitRunner.class)
-//@ExtendWith(MockitoExtension.class)
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ExtendWith(MockitoExtension.class)
 public class AuthTests {
+
     @Mock
     private ResponseErrorValidation responseErrorValidation;
 
@@ -86,15 +97,19 @@ public class AuthTests {
 
         ResponseEntity<Object> expectedResponse = ResponseEntity.ok(new MessageResponse("User registered successfully"));
 
-
         BindingResult bindingResult = mock(BindingResult.class);
-        when(bindingResult.hasErrors()).thenReturn(false);
-
+        lenient().when(bindingResult.hasErrors()).thenReturn(false);
 
         ResponseEntity<Object> actualResponse = authController.registerUser(singUpRequest, bindingResult);
 
-       // verify(userService, times(1)).createUser(singUpRequest);
+        verify(userService, times(1)).createUser(singUpRequest);
 
         assertEquals(expectedResponse.getStatusCode(), actualResponse.getStatusCode());
+
+
+
     }
+
+
+
 }
